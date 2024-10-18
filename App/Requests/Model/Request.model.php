47f148c40c6e4cd5375160ab5ba8d5822a93edbb
Lautaro <lautaroouvrard@gmail.com>
2024-10-18 18:22:16 -0300

    Añadido config.php y el deploy()

--- App/Requests/Model/Request.model.php
+++ App/Requests/Model/Request.model.php
@@ -1,10 +1,21 @@
 <?php
-
+require_once './config.php';
 class RequestModel{
     private $db;
 
     public function __construct() {
-        $this->db =new PDO('mysql:host=localhost;'.'dbname=tiendajuegos;charset=utf8', 'root', '');
+        $this->db =new PDO("mysql:host=".MYSQL_HOST . ";dbname=".MYSQL_DB.";charset=utf8", MYSQL_USER, MYSQL_PASS);
+        $this->_deploy();
+    }
+    private function _deploy() {
+        $query = $this->db->query('SHOW TABLES');
+        $tables = $query->fetchAll();
+        if(count($tables) == 0) {
+            $sql =<<<END
+
+		END;
+        $this->db->query($sql);
+        }
     }
 
     public function getRequests($id){
---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+const MYSQL_USER = 'root';
+const MYSQL_PASS = '';
+const MYSQL_DB = 'tiendajuegos';
+const MYSQL_HOST = 'localhost';
+
